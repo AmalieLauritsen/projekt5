01c341377b5394806f38755eef21cf80b270f89a
MalleLauritsen <amalie@familienlauritsen.dk>
2020-04-12 13:52:51 +0200

    Add + edit JavaScript
    '.html' & '.js'

--- js/amalie.js
+++ js/amalie.js
@@ -3,37 +3,43 @@ function changeImage(){
 	var silhouet = document.querySelector(".silhouetter");
 	if (silhouet.src.match("img/figurer/ko.png")){
 		silhouet.src = "img/figurer/koFarve.png";
-	}
-	else if (silhouet.src.match("img/figurer/broed.png")){
+	} else if (silhouet.src.match("img/figurer/broed.png")){
 		silhouet.src = "img/figurer/broedFarve.png";
-	}
-	else if (silhouet.src.match("img/figurer/toiletpaper.png")){
+	} else if (silhouet.src.match("img/figurer/toiletpaper.png")){
 		silhouet.src = "img/figurer/toiletpaperFarve.png";
-	}
-	else if (silhouet.src.match("img/figurer/hoene.png")){
+	} else if (silhouet.src.match("img/figurer/hoene.png")){
 		silhouet.src = "img/figurer/hoeneFarve.png";
 	}
 }
 
 setTimeout(changeImage, 350);
-/*
-	//FUNDET FIGUR 1, 2, 3, 4 - change page
+
+//FUNDET FIGUR 1, 2, 3, 4 - change page
 	function changePage(){
-		location.replace("./oversigt.html")
+		setTimeout(function() {
+			location.replace("./oversigt.html");
+		}, 10000);
 	}
 
-	setTimeout(changePage, 10000);
-*/
-/* OVERSIGT - cookies */
-function setCookie{
-	document.cookie = "path=/oversigt.html";
+/* OVERSIGT - localStorage */
+function checkImages(){
+	if (localStorage.getItem("fundet1") != null) {
+		document.getElementById("overbil1").src = localStorage.getItem("fundet1");
+	}
+	if (localStorage.getItem("fundet2") != null) {
+		document.getElementById("overbil2").src = localStorage.getItem("fundet2");
+	}
+	if (localStorage.getItem("fundet3") != null) {
+		document.getElementById("overbil3").src = localStorage.getItem("fundet3");
+	}
+	if (localStorage.getItem("fundet4") != null) {
+		document.getElementById("overbil4").src = localStorage.getItem("fundet4");
+	}
 }
-function getCookie{}
-function checkCookie{}
 
 //PRÆMIESIDE
 function displayGift(){
 	document.getElementById("gaveikon").classList.add("showing");
 }
 
-setTimeout(displayGift, 150);
+setTimeout(displayGift, 150)
